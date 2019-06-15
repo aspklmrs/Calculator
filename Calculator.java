ab5b097cf84b383ab309134383ce8f5296d41aa3
aspklmrs <aspklmrs@gmail.com>
2019-06-15 08:32:19 +0000

    Second

--- Calculator.java
+++ Calculator.java
@@ -6,12 +6,12 @@ public class Calculator{
         while(exit)
         {
             System.out.println("###################################################");
-            System.out.println("Enter a Nmber");
+            System.out.println("Enter a Nmber");//Inputting Data
             int a=sc.nextInt();
             System.out.println("Enter another Nmber");
             int b=sc.nextInt();
             System.out.println("###################################################");
-            System.out.println("---------------------MENU-------------------------");
+            System.out.println("---------------------MENU-------------------------");//Menu Program for Calculator
             System.out.println("1. For Addition");
             System.out.println("2. For Multiplication");
             System.out.println("3. For Subraction");
@@ -21,18 +21,18 @@ public class Calculator{
             switch(sc.nextInt())
             {
                 case 1:
-                        System.out.println("Addition :"+(a+b));
+                        System.out.println("Addition :"+(a+b));//Addition of numbers
                         break;
                 case 2:
-                        System.out.println("Multiplication :"+(a*b));
+                        System.out.println("Multiplication :"+(a*b));//Multipication of numbers
                         break;
                 case 3:
-                        System.out.println("Subraction :"+(a-b));
+                        System.out.println("Subraction :"+(a-b));//Subraction of numbers
                         break;
                 case 4:
                         if(b!=0)
                         {
-                        System.out.println("Division :"+(float)(a/b));
+                        System.out.println("Division :"+(a/(float)b));//Division  oof numbers
                     }
                         else
                         {
